bc1c1a8fe154965c75e5a20f6cd80a92571d0433
osm3rr <osm3rr@mail.com>
2025-01-25 12:03:46 -0400

    add createView and image

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,DeleteView,UpdateView
 from projects.models import Post
-# Create your views here.
-
+from django.urls import reverse_lazy
 class ListProjects(ListView):
     model=Post
     template_name="home.html"
-
 class DetailProjects(DetailView):
     model=Post
     template_name="detail.html"
+
+class CreatePageView(CreateView):
+    model=Post
+    template_name="create.html"
+    fields= ["title","description","image"]
+    success_url = reverse_lazy("home")
