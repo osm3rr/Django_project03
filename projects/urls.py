91a6075e990af446c6687f1c2ac797ac21b91e90
osm3rr <osm3rr@mail.com>
2025-02-15 11:09:58 -0400

    add welcome html

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,9 @@ from projects.views import *
 
 urlpatterns=[
     
-    path("",ListProjects.as_view(),name="home"),
+    path("",WelcomePage,name="welcome"),
+
+    path("list",ListProjects.as_view(),name="home"),
     path("detail/<int:pk>",DetailProjects.as_view(),name="detail"),
     path("create",CreatePageView.as_view(),name="create"),
     path("detail/<int:pk>/update",UpdatePageView.as_view(),name="update"),
